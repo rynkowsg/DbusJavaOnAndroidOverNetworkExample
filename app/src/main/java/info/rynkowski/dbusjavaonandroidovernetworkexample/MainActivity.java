36e72f9a70a3a41ff5f347d613b50168685df891
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2015-08-17 10:24:54 +0200

    Implemented a test notification over network

--- app/src/main/java/info/rynkowski/dbusjavaonandroidovernetworkexample/MainActivity.java
+++ app/src/main/java/info/rynkowski/dbusjavaonandroidovernetworkexample/MainActivity.java
@@ -1,12 +1,29 @@
 package info.rynkowski.dbusjavaonandroidovernetworkexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    findViewById(R.id.btn_notify).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Log.i("MainActivity", "button clicked");
+
+        new Thread(new Runnable() {
+          @Override public void run() {
+            DbusHelper dbusHelper = new DbusHelper();
+            dbusHelper.openConnection("192.168.1.112", "55555");
+            dbusHelper.showNotification("Remote Application", "This message is a test.", 10);
+            dbusHelper.closeConnection();
+          }
+        }).start();
+      }
+    });
   }
 }
